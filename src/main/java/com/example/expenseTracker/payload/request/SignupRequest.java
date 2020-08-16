d827d5f8e1c3dc8ab0e48e8f99c7c1e7dc04a0a7
macymuhia <macymuhia@gmail.com>
2020-08-16 23:45:13 +0300

    Define payloads for Spring RestController

---
+++ src/main/java/com/example/expenseTracker/payload/request/SignupRequest.java
@@ -0,0 +1,34 @@
+package com.example.expenseTracker.payload.request;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+import java.util.Set;
+
+@Getter
+@Setter
+public class SignupRequest {
+    @NotBlank
+    @Size(min = 3, max = 20)
+    private String userName;
+
+    @NotBlank
+    @Size(max = 50)
+    @Email
+    private String email;
+
+    private Set<String> role;
+
+    @NotBlank
+    @Size(min = 6, max = 40)
+    private String password;
+
+    public SignupRequest(String userName, String email, String password) {
+        this.userName = userName;
+        this.email = email;
+        this.password = password;
+    }
+}
